c32335010ea4aa867e0b19a98ecf748f2e2064c2
redDwarf03 <rddwarf03@gmail.com>
2021-12-21 22:49:26 +0100

    add verification_response.dart in library

--- lib/yubidart.dart
+++ lib/yubidart.dart
@@ -1,4 +1,5 @@
 /// Package yubidart aims to provide a easy way to use Yubico services with Yubikey.
 library yubidart;
 
+export 'src/model/verification_response.dart';
 export 'src/services/yubico_service.dart';
